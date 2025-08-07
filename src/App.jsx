cf390dc0ad26ace9364c91df16712471eb14d84f
nhhoang14 <nhhoangne@gmail.com>
2025-08-07 21:14:37 +0700

    Commit đúng tài khoản

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import { Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import Home from './pages/Home'
 import Menu from './pages/Menu'
 import About from './pages/About'
@@ -12,20 +13,22 @@ function App() {
 
   return (
     <>
-      <div className="app-container">
-        <NavBar />
-        <div className="app-content">
-            <Routes>
-              {/* Your routes will go here */}
-              <Route path="/" element={<Home />} />
-              <Route path="/menu" element={<Menu />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/services" element={<Services />} />
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
+      <AuthProvider>
+        <div className="app-container">
+          <NavBar />
+          <div className="app-content">
+              <Routes>
+                {/* Your routes will go here */}
+                <Route path="/" element={<Home />} />
+                <Route path="/menu" element={<Menu />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/contact" element={<Contact />} />
+              </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </AuthProvider>
     </>
   )
 }
